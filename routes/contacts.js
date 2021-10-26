8dc300f784e1a642a6c682ba0d276029d37ee6c6
@mae-lab-hub <asmaeallou@outlook.com>
2021-10-25 23:55:21 -0400

    Created the routes for the user collection and contact models.

---
+++ routes/contacts.js
@@ -0,0 +1,21 @@
+let express = require('express')
+let router = express.Router();
+let mongoose = require('mongoose');
+
+//connect to the contact model
+
+let Contact = require('../models/contact');
+
+//Get route for the contact model
+router.get('/',(req,res,next)=>{
+
+    Contact.find((err, ContactList)=>{
+        if(err){
+            
+            return console.error(err);           
+        }
+        else{
+            console.log(ContactList);
+        }
+    });
+});
---
+++ routes/login.js
@@ -0,0 +1,21 @@
+let express = require('express')
+let router = express.Router();
+let mongoose = require('mongoose');
+
+//connect to the user collections model
+
+let User = require('../models/user-collection');
+
+//Get route for the user collection
+router.get('/',(req,res,next)=>{
+
+    User.find((err, UserList)=>{
+        if(err){
+            
+            return console.error(err);          
+        }
+        else{
+            console.log(UserList);
+        }
+    });
+});
