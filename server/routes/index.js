c0d968d28ea73090b4af115acd075b5dc583b933
@mae-lab-hub <asmaeallou@outlook.com>
2021-10-26 19:21:22 -0400

    Seperated the logic from the routing page, created a controlled index file to handle it indstead.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,38 +1,29 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+
+let indexController = require('../controllers/index');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-  //Create a page, look inside of the index.ejs file, then pass into the page an object with a property of title with "Express"
- res.render('index', {  title: 'Home'});
-});
-
+router.get('/', indexController.displayHomepage);
 
 /* GET Home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home'});
-});
+router.get('/home', indexController.displayHomepage);
 
 /* GET About page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
-});
+router.get('/about',indexController.displayAboutPage);
 
 /* GET Products page. */
-router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects'});
-});
+router.get('/projects', indexController.displayProjectsPage);
 
 /* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
-});
+router.get('/services', indexController.displayServicesPage);
 
 /*GET Contact Us*/
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
-});
+router.get('/contact', indexController.displayContactPage);
+
+/*GET Contact Us*/
+router.get('/login', indexController.displayContactPage);
 
 
 module.exports = router;
