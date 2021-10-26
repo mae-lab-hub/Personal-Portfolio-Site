8c90a345c2e8af1e37cc453dfca8022fff838de0
@mae-lab-hub <asmaeallou@outlook.com>
2021-10-26 19:16:01 -0400

    Added code to give a warning about deletion.

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -10,7 +10,20 @@ Date           : 29-09-21
 
     function Start()
     {
-        console.log("App has started.....")
+        console.log("App has started.....");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+
+        for (button of deleteButtons){
+
+            button.addEventListener('click',(event)=>{
+
+                if(!confirm("Are you sure?")){
+                    event.preventDefault();
+                    window.location.assign('/book-list');   
+                }
+            });
+        }
     }
 
     window.addEventListener("load", Start)
