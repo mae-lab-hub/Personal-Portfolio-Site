78831776b90ba8f9143d5de399c9b10dfabe50b5
@mae-lab-hub <asmaeallou@outlook.com>
2021-10-26 18:02:30 -0400

    Added the get and post requests for updating and deleting the contact list

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -15,20 +15,76 @@ router.get('/',(req,res,next)=>{
             return console.error(err);           
         }
         else{
-            console.log(contactList);
-
-            res.render('partials/contacts',{title:"Contacts", ContactList:contactList});     
+            
+            res.render('contact/list',{title:"Contacts", ContactList:contactList});     
         }
     });
 });
 
-module.exports = router;
 
 
-//Add Contact - Create
+//Edit Contact - Update operation
+//Get route for displaying the page
+router.get('/update:id',(req,res,next)=>{
 
+    let id = req.params.id;
 
-//Edit Contact - Update
+    Contact.findById(id,(err, contactToEdit)=>{
 
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.render('contact/update',{title:'Update Contact',  contact:contactToEdit});
+        }
+    });
+})
+
+//Post for processing the page
+router.post('/update:id',(req,res,next)=>{
+
+    let id = req.params.id;
+
+    let updateContact = Contact({
+        "_id": id,
+        "contact_name": req.body.contact_name,
+        "contact_number": req.body.contact_number,
+        "email": req.body.email
+    });
+
+    Contact.updateOne({_id:id}, updateContact,(err)=>{
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            //refresh contact list
+            res.redirect('/contact-list');
+        }
+    });   
+});
 
 //Delete contact
+//Get route to perform deletion
+router.get('/delete:id',(req,res,next)=>{
+
+    let id = req.params.id;
+
+    Contact.remove({_id:id},(err)=>{
+
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+
+            //refresh contact list
+            res.redirect('/contact-list');
+        }
+
+    });
+});
+
+
+module.exports = router;
