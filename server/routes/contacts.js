565fbc1f2471029f251d9a6ead74ee813ccaf59a
@mae-lab-hub <asmaeallou@outlook.com>
2021-10-29 02:24:40 -0400

    Added gaurds to limit access to the contact list.

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -2,27 +2,37 @@ let express = require('express')
 let router = express.Router();
 let mongoose = require('mongoose');
 
-//connect to the contact model
-
-let Contact = require('../models/contact');
+let passport = require('passport');
 
 let contactController = require('../controllers/contacts')
 
+//helper function for gaurd purposes
+function requireAuth(req,res,next){
+
+    //if the user is authenticated
+    if(!req.isAuthenticated()){
+
+        return res.redirect('/login');
+    }
+    next();
+}
+
+
 //Get route for the contact model
-router.get('/', contactController.displayContactList);
+router.get('/', requireAuth, contactController.displayContactList);
 
 //Edit Contact - Update operation
 
 //Get route for displaying the page
-router.get('/update/:id', contactController.displayUpdatePage);
+router.get('/update/:id', requireAuth, contactController.displayUpdatePage);
 
 //Post for processing the page
-router.post('/update/:id', contactController.processUpdatePage);
+router.post('/update/:id', requireAuth, contactController.processUpdatePage);
 
 //Delete contact
 
 //Get route to perform deletion
-router.get('/delete/:id', contactController.performDelete);
+router.get('/delete/:id', requireAuth, contactController.performDelete);
 
 
 module.exports = router;
