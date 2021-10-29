33a03dd1ea81cc0afca5487af14d347243420486
@mae-lab-hub <asmaeallou@outlook.com>
2021-10-28 23:34:19 -0400

    Renamed file, and added three new features.

---
+++ server/models/user.js
@@ -0,0 +1,48 @@
+let mongoose = require('mongoose');
+let passportLocalmongoose = require('passport-local-mongoose')
+//creating a model class
+let User =mongoose.Schema({
+
+   username:{
+
+     type: String,
+     default: "",
+     trim: true,
+     required: "username is required"
+   }
+   ,
+   email:{
+
+     type: String,
+     default: "",
+     trim: true,
+     required: "email is required"
+   },
+   displayName:{
+
+     type: String,
+     default: "",
+     trim: true,
+     required: "Display name is required"
+   },
+   created:{
+
+     type: Date,
+     default: Date.now
+   },
+   updated:{
+
+     type: Date,
+     default: Date.now
+   }
+
+},
+{
+    collection:"users"
+});
+
+let options = ({missingPasswordError: "Wrong / Missing Password"});
+User.plugin(passportLocalMongoose, options);
+
+//return the model
+module.exports.User = mongoose.model('User', User);
