c32981894551b40d98ff83ef4912ceebf6ad3b18
@mae-lab-hub <asmaeallou@outlook.com>
2021-10-30 03:03:33 -0400

    Changed URI to an online database.

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports ={
 
-    "URI":"mongodb://localhost/personal-portfolio" 
+   // "URI":"mongodb://localhost/personal-portfolio",
+    "URI": "mongodb+srv://Mae:JuFNpEnTI9iiGumb@cluster0.wklvn.mongodb.net/personal-portfolio?retryWrites=true&w=majority"
 }
